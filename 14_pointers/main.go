d71475cea4a565082e8c1f5027aa271395bfc8f9
Mehedi Mosharrof <mmraaj378@gmail.com>
2025-11-08 11:19:58 +0600

    practice the pointer

--- 14_pointers/main.go
+++ 14_pointers/main.go
@@ -1,9 +1,15 @@
 package main
 
-func changeNum() {
+import "fmt"
 
+func changeNum(num *int) { // *int refers to the pointer/ memory location of argument
+	fmt.Println("Before change:", *num)
+	*num = 55 // dereferencing the pointer to change the value at that memory location
+	fmt.Println("Inside function:", *num)
 }
 
 func main() {
-
+	num := 10
+	changeNum(&num) // passing the memory address of num using & operator
+	fmt.Println("Outside function:", num)
 }
